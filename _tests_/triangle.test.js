23da0d0d562445387119dfbb6023dbf845d9ccb8
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-31 22:37:03 -0700

    updated testing with jest and passed 5 tests

---
+++ _tests_/triangle.test.js
@@ -0,0 +1,43 @@
+import Triangle from './../src/triangle.js';
+
+describe('Triangle', () => {
+
+  test('should correctly create a triangle object with three lengths', () => {
+    const triangle = new Triangle(2,4,5);
+    expect(triangle.side1).toEqual(2);
+    expect(triangle.side2).toEqual(4);
+    expect(triangle.side3).toEqual(5);
+  });
+});
+
+describe('Triangle', () => {
+
+  test('should correctly determine whether three lengths are not a triangle', () => {
+    const notTriangle = new Triangle(3,9,22);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
+});
+
+describe('Triangle', () => {
+
+  test('should correctly determine whether three lengths make a scalene triangle', function() {
+    const scalTriangle = new Triangle(4,5,7)
+    expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  });
+});
+
+describe('Triangle', () => {
+
+  test('should correctly determine whether three lengths make an isosceles triangle', () => {
+    const isocTriangle = new Triangle(5,5,7)
+    expect(isocTriangle.checkType()).toEqual("isosceles triangle");
+  });
+});
+
+describe('Triangle', () => {
+
+  test('should correctly determine whether three lengths make an equilateral triangle', () => {
+    const equiTriangle = new Triangle(5,5,5)
+    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
+});
