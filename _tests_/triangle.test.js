d46b49a3ab7d7312d066a4e544af16fb65ec7750
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-01 23:05:46 -0700

    added refactored js to the triangle.js. This was a class change and removed the prototype function. Added rectangle.js with added rectangle.text.js. Tested rectangle sides and passed.

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -1,4 +1,4 @@
-import Triangle from './../src/triangle.js';
+import Triangle from './../src/js/triangle.js';
 
 describe('Triangle', () => {
 
@@ -8,33 +8,31 @@ describe('Triangle', () => {
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
   });
-});
 
-describe('Triangle', () => {
+
+
 
   test('should correctly determine whether three lengths are not a triangle', () => {
     const notTriangle = new Triangle(3,9,22);
     expect(notTriangle.checkType()).toEqual("not a triangle");
   });
-});
 
-describe('Triangle', () => {
+
+
 
   test('should correctly determine whether three lengths make a scalene triangle', function() {
     const scalTriangle = new Triangle(4,5,7)
     expect(scalTriangle.checkType()).toEqual("scalene triangle");
   });
-});
 
-describe('Triangle', () => {
+  
 
   test('should correctly determine whether three lengths make an isosceles triangle', () => {
     const isocTriangle = new Triangle(5,5,7)
     expect(isocTriangle.checkType()).toEqual("isosceles triangle");
   });
-});
 
-describe('Triangle', () => {
+
 
   test('should correctly determine whether three lengths make an equilateral triangle', () => {
     const equiTriangle = new Triangle(5,5,5)
